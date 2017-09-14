cbea598a231e7e49ad22ab045408204685e4084e
Tuule Põldsaar <dyyle@users.noreply.github.com>
2017-09-14 11:31:14 +0300

    lisatud muutuja ja IF lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,24 @@
+<?php
+	//muutujad
+	$myName = "Tuule";
+	$myFamilyName = "Põldsaar";
+	
+	//hindan päeva osa (võrdlemine < > <= >= == !=)
+	$hourNow = date("H");
+	$partOfDay = "";
+	if ($hourNow < 8) {
+		$partOfDay = "varajane hommik";
+	}
+	if ($hourNow >= 8 and $hourNow < 16) {
+		$partOfDay = "koolipäev";
+	}
+	if ($hourNow > 16) {
+		$partOfDay = "vaba aeg";
+	}
+	//echo $partOfDay
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,12 +26,14 @@
 	<title> Tuule veebiprogrammeerimine </title>
 </head>
 <body>
-	<h1>Tuule Põldsaar, veebiprogrammeerimine</h1>
+	<h1><?php echo $myName ." " .$myFamilyName; ?>, veebiprogrammeerimine</h1>
 	<p>tekst blablbalba</p>
 	<?php
-		echo "Algas PHP õppimine.";
-	?>
-	
+		echo "<p>Algas PHP õppimine.</p>";
+		echo "<p>Täna on ";
+		echo date("d.m.Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
+		echo ", hetkel on " .$partOfDay .".</p>";
+		?>
 	
 </body>
 </html>

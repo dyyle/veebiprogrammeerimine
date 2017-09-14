8fd972b833a13a3990187c5a7cf4b12316663bfc
Tuule Põldsaar <dyyle@users.noreply.github.com>
2017-09-14 10:46:43 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title> Tuule veebiprogrammeerimine </title>
+</head>
+<body>
+	<h1>Tuule Põldsaar, veebiprogrammeerimine</h1>
+	<p>tekst blablbalba</p>
+	<?php
+		echo "Algas PHP õppimine.";
+	?>
+	
+	
+</body>
+</html>
